f07248be17d808db2afdd81a17c53211ccdf1672
Michael Kutz <michael.kutz@rewe-digital.com>
2017-03-21 13:21:48 +0100

    Added demonstration for logging tests with logback.

---
+++ testing-log/logback/src/main/groovy/de/assertagile/demonstration/howtotest/log/LoggingClass.groovy
@@ -0,0 +1,11 @@
+package de.assertagile.demonstration.howtotest.log
+
+import groovy.util.logging.Slf4j
+
+@Slf4j
+class LoggingClass {
+
+    LoggingClass() {
+        log.debug("Hello")
+    }
+}
---
+++ testing-log/logback/src/test/groovy/de/assertagile/demonstration/howtotest/log/LoggingClassTest.groovy
@@ -0,0 +1,44 @@
+package de.assertagile.demonstration.howtotest.log
+
+import ch.qos.logback.classic.Level
+import ch.qos.logback.classic.Logger
+import ch.qos.logback.classic.spi.LoggingEvent
+import ch.qos.logback.core.Appender
+import org.slf4j.LoggerFactory
+import spock.lang.Specification
+
+class LoggingClassTest extends Specification {
+
+    List<LoggingEvent> loggingEvents = []
+
+    Appender<LoggingEvent> appenderMock = Mock() {
+        _ * getName() >> "MOCK"
+        _ * doAppend({ loggingEvents << it })
+    }
+
+    void setup() {
+        Logger root = LoggerFactory.getLogger(Logger.ROOT_LOGGER_NAME) as Logger
+        root.addAppender(appenderMock)
+    }
+
+    void "logging class should log"() {
+        given:
+        LoggingEvent event = null
+
+        when:
+        new LoggingClass()
+
+        then:
+        1 * appenderMock.doAppend({ event = it })
+        event.level == Level.DEBUG
+    }
+
+    void "teo"() {
+        when:
+        new LoggingClass()
+
+        then:
+        loggingEvents.size() == 1
+        loggingEvents.find { it.level == Level.DEBUG && it.message == "Hello" }
+    }
+}
